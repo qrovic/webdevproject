5bfea623cf460cc254c301c6b7a382b138eca5fd
Rui | 罗尔 <quilantangrovic@gmail.com>
2023-10-31 20:47:36 +0800

    fix in add.php

--- functions/add.php
+++ functions/add.php
@@ -1,35 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Data</title>
-</head>
-<body>
-    <?php
-        include_once("../dbConnections/mysqlconfig_connection.php");
+<?php
+    include_once("../dbConnections/mysqlconfig_connection.php");
 
-        if(isset($_POST['Submit'])){ 
-            $code = $_POST['code']; 
-            $name = $_POST['name'];
+    if(isset($_POST['submit'])){ 
+        $code = $_POST['code']; 
+        $name = $_POST['name'];
 
-            if(empty($code) || empty($name)){
-                if(empty($code)){
-                    echo "<font color='red'> Subject Code field is empty.</font><br/>";
-                }
-                if(empty($name)){
-                    echo "<font color='red'> Subject Name field is empty.</font><br/>";
-                }
-                
-                echo "<br/><a href='javascript:self.history.back();'>Go back</a>";
-                
+        if(empty($code) || empty($name)){
+            if(empty($code)){
+                echo "<font color='red'> Subject Code field is empty.</font><br/>";
             }
-            else{
-                $result = mysqli_query($dbc, "INSERT INTO tblsubjects(subject_code, subject_name) VALUES ('$code','$name')");
+            if(empty($name)){
+                echo "<font color='red'> Subject Name field is empty.</font><br/>";
+            }
+            
+            echo "<br/><a href='javascript:self.history.back();'>Go back</a>";
+            
+        }
+        else{
+            $result = mysqli_query($dbc, "INSERT INTO tblsubjects(subject_code, subject_name) VALUES ('$code','$name')");
+            if($result) {
                 echo "<font color='green'> Data added successfully.</font>";
                 echo "<br/><a href='../index.php'>View Results</a>";
-            }  
+            } else {
+                echo "<font color='red'> Error: " . mysqli_error($dbc) . "</font>";
+            }
+            mysqli_close($dbc); // Close the database connection here
         }
-    ?>
-</body>
-</html>
+    }
+?>
