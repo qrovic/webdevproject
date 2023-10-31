41cb19a5b5b3716788e236300b458c95bb3a1bae
Rui | 罗尔 <quilantangrovic@gmail.com>
2023-10-31 19:57:39 +0800

    change table name

--- functions/add.php
+++ functions/add.php
@@ -9,26 +9,27 @@
     <?php
         include_once("../dbConnection/mysqliconfig_connection.php");
 
-        if(isset($POST['Submit'])){
-            $code=$_POST['code']
-            $name=$_POST['name']
-        
+        if(isset($_POST['Submit'])){ 
+            $code = $_POST['code']; 
+            $name = $_POST['name'];
+
             if(empty($code) || empty($name)){
                 if(empty($code)){
-                    echo "<font color='red'> Subject Code filed is empty.</font><br/>";
+                    echo "<font color='red'> Subject Code field is empty.</font><br/>";
                 }
                 if(empty($name)){
-                    echo "<font color='red'> Subject Name filed is empty.</font><br/>";
+                    echo "<font color='red'> Subject Name field is empty.</font><br/>";
                 }
                 
                 echo "<br/><a href='javascript:self.history.back();'>Go back</a>";
                 
             }
             else{
-                $result=mysqli_query($dbc, "INSERT INTO tblsubject(subject_code, subject_name) VALUES ('$code','$name')");
-                echo "<font color='green'> Data added successfully.";
-                echo "<br/><a href='../index.php'>View Result</a>";
+                $result = mysqli_query($dbc, "INSERT INTO tblsubjects(subject_code, subject_name) VALUES ('$code','$name')");
+                echo "<font color='green'> Data added successfully.</font>";
+                echo "<br/><a href='../index.php'>View Results</a>";
             }
+            mysqli_close($dbc); 
         }
     ?>
 </body>
