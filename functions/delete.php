c7293ba0fb7ca168a9c42c263b260050c661c131
Rui | 罗尔 <quilantangrovic@gmail.com>
2023-10-31 20:46:15 +0800

    fix in delete

--- functions/delete.php
+++ functions/delete.php
@@ -1,6 +1,7 @@
 <?php
-    include_once("../dbConnection/mysqlconfig_connection.php");
-    $id=$_get['id'];
+    include_once("../dbConnections/mysqlconfig_connection.php");
+    $id=$_GET['id'];
 
-    mysqli_query($dbc,"DELETE FROM tblsubject WHERE subject_id='$id'");
+    mysqli_query($dbc,"DELETE FROM tblsubjects WHERE subject_id='$id'");
     header("location:../index.php")
+?>
