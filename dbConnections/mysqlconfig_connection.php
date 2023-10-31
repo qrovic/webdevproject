37ed1b20cef90e9b3fb8005611107982a95b4755
Rui | 罗尔 <quilantangrovic@gmail.com>
2023-10-31 20:03:30 +0800

    fix typographical erro

--- dbConnections/mysqlconfig_connection.php
+++ dbConnections/mysqlconfig_connection.php
@@ -5,5 +5,5 @@
     DEFINE('DB_NAME', 'dbschedule');
 
     $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-    OR dies('Could not connect to Mysql: ' . mysql_connect_error());
+    or die('Could not connect to MySQL: ' . mysqli_connect_error()); 
 ?>
